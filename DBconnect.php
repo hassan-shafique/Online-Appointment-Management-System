7e083d3c6c56371ebbbf2b16c24e18a7f8b78a31
hassan-shafique <73396761+hassan-shafique@users.noreply.github.com>
2025-01-08 17:35:40 +0500

    Update DBconnect.php

--- DBconnect.php
+++ DBconnect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost','root','','appointment');
+$conn = mysqli_connect('testapp-server.mysql.database.azure.com:3306','testapp-server.mysql.database.azure.com','Calrom123','testapp-database');
 
 // Check connection
 if ($conn->connect_error) {
